5e447b7bd7c1f62ec505eae6218f418e78febb0a
fMuchka <37208551+fMuchka@users.noreply.github.com>
2020-08-30 21:07:08 +0200

    load file component
    Load file component created and connected to Vue and Vuex.
    
    NOTE: Styling needed in future.

--- script/main.js
+++ script/main.js
@@ -1,3 +1,40 @@
+// app store
+const store = new Vuex.Store({
+    state: {
+        quiz: null
+    },
+
+    mutations: {
+        // load quiz via file upload
+        loadQuiz (state) {
+            let reader = new FileReader();
+            reader.onload = () =>{
+                state.quiz = JSON.parse(event.target.result); 
+            };
+
+            reader.readAsText(event.target.files[0]); 
+        }
+    },
+
+    getters: {
+        quiz: state => {
+            return state.quiz;
+        }
+    }
+});
+
+
+
+
 window.onload = () => {
-    console.log('Hello World!')
+    // file upload component
+    let quizLoad = new Vue({
+        el: '#quizLoadInput',
+        store,
+        methods : {
+            loadFile : () => {
+                store.commit('loadQuiz');
+            }
+        }
+    });
 }
