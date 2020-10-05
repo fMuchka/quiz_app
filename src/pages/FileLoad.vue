b5a9135a4002c7221071ab06fd28fa464a6a2d55
fMuchka <37208551+fMuchka@users.noreply.github.com>
2020-10-05 22:00:29 +0200

    test

--- src/pages/FileLoad.vue
+++ src/pages/FileLoad.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
+<<<<<<< HEAD
     <h2 class="redcolor">
       Here we load files
     </h2>
+=======
+    <img id="logo"
+         src="/src/assets/sviniPivemNezarmoutis_logo.png" 
+         alt="nope"
+    >
+>>>>>>> parent of 579e003... Finalized FileLoad.vue test
 
     <input id="quizLoadInput"
            type="file" 
            name="quiz-load"
+<<<<<<< HEAD
            v-on:change="loadFile()" />
+=======
+           title=" "
+           v-on:change="loadFile()" />
+
+    <label for="quizLoadInput"> Open Quiz </label>
+>>>>>>> parent of 579e003... Finalized FileLoad.vue test
     <div>
       <b>Loaded data:</b>
       {{ loadedQuizData }}
@@ -40,8 +54,38 @@ export default {
 };
 </script>
 <style scoped>
+<<<<<<< HEAD
   .redcolor {
     color: #FF0000;
+=======
+  #logo{
+    height: 30vw;
+    display: flex;
+    margin: auto;
+  }
+
+  #quizLoadInput{
+    display: none;
+  }
+
+  label[for="quizLoadInput"] {
+    display: flex;
+    margin: auto;
+    color: white;
+    border-radius: 5px;
+    border: 1px solid var(--secondary-color);
+    background-color: var(--secondary-color);
+    width: 25vw;
+    font-size: 50px;
+    place-content: center;
+    padding: 10px 0;
+  }
+
+  label[for="quizLoadInput"]:hover {
+    cursor: pointer;
+    background-color: FCBF49;
+    color: 003049;
+>>>>>>> parent of 579e003... Finalized FileLoad.vue test
   }
 </style>
 
