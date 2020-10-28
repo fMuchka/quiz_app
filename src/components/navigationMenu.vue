56fdb7537cb9b752c0a4b145ae3638e336dcb2de
fMuchka <37208551+fMuchka@users.noreply.github.com>
2020-10-28 13:59:39 +0100

    01
    navigation menu component + css

---
+++ src/components/navigationMenu.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+      <input id="menu-icon" @click="changeVisibility()" type="checkbox">
+      <label for="menu-icon">
+        <div></div>
+        <div></div>
+        <div></div>
+      </label>
+      
+
+      <div id="menu" :style="menuStyle">
+        <!-- use router-link component for navigation. -->
+        <!-- specify the link by passing the `to` prop. -->
+        <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+        <router-link to="/fileload">Otevřít kvíz</router-link>
+        <router-link to="/teamcreate">Tvorba týmů</router-link>
+      </div>
+        
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      hidden: true
+    }
+  },
+
+  computed: {
+    isMenuHidden: {
+      get() {
+        return this.hidden;
+      },
+      set(newState) {
+        this.hidden = newState;
+      }
+    },
+    menuStyle() {
+      return this.hidden ? 'left:-20%' : 'left:0px';
+    }
+  },
+
+  methods: {
+    changeVisibility(){
+      this.isMenuHidden = !this.isMenuHidden;
+    }
+  }
+}
+</script>
+
+<style>
+.router-link-active{
+  color: 003049 !important;
+  background-color: var(--main-color) !important;
+}
+
+label[for="menu-icon"] > div{
+  width: 35px;
+  height: 5px;
+  background-color: var(--secondary-color);
+  margin: 6px 0;
+}
+
+label[for="menu-icon"]{
+  position: absolute;
+  z-index: 5;
+}
+
+#menu-icon:hover ~ label[for="menu-icon"] > div{
+  background-color: white;
+}
+
+#menu-icon{
+    background: none;
+    border: none;
+    outline: none;
+    display: none;
+}
+
+#menu-icon:checked ~ label[for="menu-icon"] > div{
+   background-color: white;
+}
+
+label[for="menu-icon"]:hover{
+  cursor: pointer;
+}
+
+#menu{
+    display: flex;
+    position: absolute;
+    height: 100vh;
+    top: 0;
+    background-color: D62828;
+    flex-direction: column;
+    width: 20%;
+    transition: left ease-in 0.5s;
+    justify-content: center;
+}
+
+#menu > a{
+  display: flex;
+  width: 80%;
+  height: 45px;
+  margin: 5px auto;
+  place-content: center;
+  align-items: center;
+  background-color: var(--secondary-color);
+  color: white;
+  text-decoration: none;
+}
+
+#menu > a:hover {
+  background-color: var(--main-color);
+}
+</style>
