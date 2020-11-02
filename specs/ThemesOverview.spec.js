d31a6c768d74d6d83f3f4a529841d2172263558a
fMuchka <37208551+fMuchka@users.noreply.github.com>
2020-11-02 22:44:33 +0100

    ThemesOverview page tests

---
+++ specs/ThemesOverview.spec.js
@@ -0,0 +1,42 @@
+import Vuex from "vuex";
+import { mount, createLocalVue } from "@vue/test-utils";
+
+import ThemesOverview from "../src/pages/ThemesOverview.vue"
+import mainStore from "../src/store.js";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe("ThemesOverview Page:", () => {
+    let store;
+
+    beforeEach(() => {
+        mainStore.state.quiz = { theme: [{ title: "1" }, { title: "2" }, { title: "3" }] };
+        store = new Vuex.Store(mainStore);
+    });
+
+    it("matches snapshot", () => {
+        const wrapper = mount(ThemesOverview, {
+            localVue,
+            store
+        });
+
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it("creates Themes based on Store.state.quiz", () => {
+        const wrapper = mount(ThemesOverview, {
+            localVue,
+            store
+        });
+
+        const testThemes = store.state.quiz.theme;
+        const pageElements = wrapper.vnode.children;
+
+        expect(pageElements.length).toBe(testThemes.length);
+
+        for (let i = 0; i < pageElements.length; i++){
+            expect(pageElements[i].children[0].text.includes(testThemes[i].title)).toBeTruthy()
+        }
+    });
+})
